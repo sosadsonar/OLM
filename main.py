27c0feaea358ed351f851fd93108dad134fa4f8e
sosadsonar <hoangnhat15305@gmail.com>
2024-02-20 22:52:04 +0700

    Simplify code

--- main.py
+++ main.py
@@ -4,28 +4,6 @@ import os
 import sys
 import argparse
 import re
-import pdfkit
-import time
-from pylatexenc.latex2text import LatexNodes2Text
-
-
-# Get current binaries.
-def get_cur_dir():
-    cur_dir = os.path.dirname(os.path.abspath(__file__))
-    # Add wkhtmltopdf binaries.
-    if sys.platform.startswith("linux"):
-        path_wkhtmltopdf = os.path.join(cur_dir, "wkhtmltopdf")
-        config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
-        return config
-    
-    elif sys.platform.startswith("win32"):
-        path_wkhtmltopdf = os.path.join(cur_dir, "wkhtmltopdf.exe")
-        config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
-        return config
-        
-    else:
-        print("[Error] Your OS is not supported! Exit.")
-        return
 
 
 # Handle path & folder.
@@ -64,22 +42,18 @@ def decode_file(fn, folder):
             return
         
         else:
-            print("[INFO] File supported! Start decoding.")
-            time.sleep(0.5)    
+            print("[INFO] File supported! Start decoding.")    
     else:
         print("[INFO] File supported! Start decoding.")
-        time.sleep(0.5)
     
     # Decode QaA then store it in a list.
     print("[INFO] Decoding questions and answers.")
-    time.sleep(0.5)
     decodedQaAs = []
     for encodedQaA in encodedQaAs:
         decodedQaAs.append(html.unescape((base64.b64decode(encodedQaA)).decode("utf-8")))
 
     # Decode HTML and apply color format for correct answer.
     print("[INFO] Applying color format.")
-    time.sleep(0.5)
     split = []
     questionNumber = 0
 
@@ -94,72 +68,13 @@ def decode_file(fn, folder):
         os.mkdir(folder)
     except FileExistsError:
         pass
-    
-    print("[INFO] Writing to temporary file.")
-    time.sleep(0.5)
     os.chdir(folder)
     with open("decode.htm", "w", encoding="utf-8") as file2:
         for i in split:
             file2.write(f"Câu {questionNumber + 1}:\n")
             file2.write(i + "\n")
             questionNumber += 1
-    
-    # Decodes LaTeX encoding in a file.
-    add_fraction_bar_for_fractions("decode.htm")
-
-
-def add_fraction_bar_for_fractions(input_file):
-    # Add fraction bar for fractions.
-    fractions = []
-    
-    with open(input_file, "r", encoding="utf-8") as f:
-        fractions_sign = re.findall(r"(?<=\\dfrac).+?(?=\$)", f.read())
-        if not fractions_sign:
-            parse_latex(input_file)
-            return
-        
-        else: 
-            fractions.append(re.sub(r"(?<=\})(?=\{)", r"/", f.read()))   
-    
-    with open(input_file, "w", encoding="utf-8") as f:
-        for i in fractions:
-            f.write(i)
-    
-    parse_latex(input_file)
-    
-
-def parse_latex(input_file):
-    print("[INFO] Parsing LaTex (if available).")
-    time.sleep(0.5)
-    with open(input_file, "r", encoding="utf-8") as f:
-        latex_code = f.read()
-        html_code = LatexNodes2Text().latex_to_text(latex_code)
-
-    with open(input_file, "w", encoding="utf-8") as f:
-        f.write(html_code)
-          
-    # Convert the HTML file "input.htm" to the PDF file "output.pdf".
-    convert_htm_to_pdf(input_file, "Decoded.pdf")
-
-
-def convert_htm_to_pdf(htm_file, pdf_file):
-    print("[INFO] Converting HTML file to PDF.")
-    time.sleep(0.5)
-    # Options for wkhtmltopdf.
-    options = {
-        "encoding": "UTF-8"
-    }
-    
-    get_cur_dir()
-    pdfkit.from_file(htm_file, pdf_file, options = options, configuration=get_cur_dir())
-    print(f"[INFO] File decoded. Output file is in {os.path.abspath(os.curdir)}.")
-    
-    try:
-        os.remove(htm_file)
-    except PermissionError:
-        print("[Warning] Cannot delete decode.htm file.")
-    else:
-        return
+    print(f"[INFO] Done! File is stored in {os.path.join(folder, 'decode.htm')}")
     
        
 if __name__ == "__main__":
